81a6662255fb7336551b62477144f68096b33b19
zql9000 <zql9000@users.noreply.github.com>
2018-05-04 19:20:29 -0300

    Primer version

---
+++ LeerRegistro/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.btnExaminar = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblValorBoca = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblValorOjos = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.lblValorModos = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(161, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Path del archivo binario:"
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(179, 34)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(286, 22)
+        Me.txtPath.TabIndex = 1
+        '
+        'btnExaminar
+        '
+        Me.btnExaminar.Location = New System.Drawing.Point(473, 34)
+        Me.btnExaminar.Name = "btnExaminar"
+        Me.btnExaminar.Size = New System.Drawing.Size(32, 23)
+        Me.btnExaminar.TabIndex = 2
+        Me.btnExaminar.Text = "..."
+        Me.btnExaminar.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Timer1
+        '
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblValorBoca)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 80)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(157, 227)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Registro de la Boca"
+        '
+        'lblValorBoca
+        '
+        Me.lblValorBoca.AutoSize = True
+        Me.lblValorBoca.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblValorBoca.Location = New System.Drawing.Point(40, 77)
+        Me.lblValorBoca.Name = "lblValorBoca"
+        Me.lblValorBoca.Size = New System.Drawing.Size(87, 95)
+        Me.lblValorBoca.TabIndex = 0
+        Me.lblValorBoca.Text = "0"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblValorOjos)
+        Me.GroupBox2.Location = New System.Drawing.Point(179, 80)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(153, 227)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Registro de los Ojos"
+        '
+        'lblValorOjos
+        '
+        Me.lblValorOjos.AutoSize = True
+        Me.lblValorOjos.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblValorOjos.Location = New System.Drawing.Point(39, 77)
+        Me.lblValorOjos.Name = "lblValorOjos"
+        Me.lblValorOjos.Size = New System.Drawing.Size(87, 95)
+        Me.lblValorOjos.TabIndex = 1
+        Me.lblValorOjos.Text = "0"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.lblValorModos)
+        Me.GroupBox3.Location = New System.Drawing.Point(352, 80)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(153, 227)
+        Me.GroupBox3.TabIndex = 4
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Registro de modos"
+        '
+        'lblValorModos
+        '
+        Me.lblValorModos.AutoSize = True
+        Me.lblValorModos.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblValorModos.Location = New System.Drawing.Point(39, 77)
+        Me.lblValorModos.Name = "lblValorModos"
+        Me.lblValorModos.Size = New System.Drawing.Size(87, 95)
+        Me.lblValorModos.TabIndex = 1
+        Me.lblValorModos.Text = "0"
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(521, 319)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExaminar)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(539, 364)
+        Me.MinimumSize = New System.Drawing.Size(539, 364)
+        Me.Name = "frmPrincipal"
+        Me.Text = "Prueba de lectura de archivo binario"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnExaminar As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblValorBoca As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblValorOjos As System.Windows.Forms.Label
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblValorModos As System.Windows.Forms.Label
+
+End Class
---
+++ LeerRegistro/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class frmPrincipal
+    'Defino la estructura
+    Private Structure Registro
+        Public Id As Integer
+        Public Dato As Integer
+    End Structure
+
+    'Defino la variable que voy a utilizar para abrir el archivo
+    Private oArchivo As FileStream
+    Private reader As BinaryReader
+
+    Private Sub frmPrincipal_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        OpenFileDialog1.FileName = "Eventos.dat"
+    End Sub
+
+    Private Sub btnExaminar_Click(sender As System.Object, e As System.EventArgs) Handles btnExaminar.Click
+        'Abro el cuadro de dialogo para abrir un archivo
+        If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            'Si presiono OK, muestro el nombre del archivo y lo abro
+            txtPath.Text = OpenFileDialog1.FileName
+            oArchivo = File.Open(txtPath.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+            reader = New BinaryReader(oArchivo, Encoding.Default)
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        'Como el timer se ejecuta todo el tiempo, verifico si se puede leer el archivo
+        If Not IsNothing(oArchivo) AndAlso oArchivo.CanRead Then
+            LeerValoresEnArchivo()
+        End If
+    End Sub
+
+    Private Sub LeerValoresEnArchivo()
+        'Obtengo los datos desde el archivo que ya está abierto y lo muestro en el formulario
+        Dim oDatoBoca As Registro
+        Dim oDatoOjos As Registro
+        Dim oDatoModos As Registro
+
+        'Muevo el puntero de lectura al principio del archivo
+        reader.BaseStream.Position = 0
+
+        'Leo los datos int32 en el orden correcto
+        Try
+            oDatoBoca.Id = reader.ReadInt32()
+            oDatoBoca.Dato = reader.ReadInt32()
+            oDatoOjos.Id = reader.ReadInt32()
+            oDatoOjos.Dato = reader.ReadInt32()
+            oDatoModos.Id = reader.ReadInt32()
+            oDatoModos.Dato = reader.ReadInt32()
+
+            'Actualizo los datos en el formulario
+            lblValorBoca.Text = oDatoBoca.Dato
+            lblValorOjos.Text = oDatoOjos.Dato
+            lblValorModos.Text = oDatoModos.Dato
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
